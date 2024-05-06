c5dd677d50c3b6e77b2f56a88260f0c5275c33d7
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-05-06 21:03:24 +0000

    Added Functionality to the Tab Bar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function openTab(name) {
-  console.log("OPENING TAB " + name);
+function openPage(newPage) {
+  var i, page;
+  page = document.getElementsByClassName("page");
+  for (i = 0; i < page.length; i++) {
+    page[i].style.display = "none";
+  }
+document.getElementById(newPage).style.display = "block";
 }
+document.getElementById("defaultOpen").click();
