083841d13bf603d2d3c59d93d61414b0774c7de5
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-05-07 17:17:31 +0000

    WIP upgraded css Nav bar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function openPage(newPage) {
+var currentPage = "Home";
+function openPage(newPage, elmnt) {
   var i, page;
   page = document.getElementsByClassName("page");
   for (i = 0; i < page.length; i++) {
     page[i].style.display = "none";
   }
-document.getElementById(newPage).style.display = "block";
+  document.getElementById(newPage).style.display = "block";
+
+  for (var i = 0; i <) {
+    document.getElementsByClassName(square).style.backgroundColor = "red";
+  }
+
+
+
+  currentPage = newPage;
+  elmnt.style.backgroundColor = "#707eff";
 }
 document.getElementById("defaultOpen").click();
