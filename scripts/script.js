abee78a4d8c7d632734a35ee9b32f6a5680358e1
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-07-03 22:52:31 +0000

    WIP - Working JSON storage of user pass age questcoins and backgroudns, info is displayed in User tab, CSS is needed it looks awful, attempted to add invalid user or pass to index but the animation and organizational elements are bad

--- scripts/script.js
+++ scripts/script.js
@@ -52,33 +52,38 @@ function equipBackground(background) {
 }
 
 
-
-
-
-
-
-
+function loadUserHTML() {
+  
+  document.getElementById("User_Username").innerHTML += sessionStorage.getItem("Current_User");
+  console.log(sessionStorage.getItem("Current_Password"));
+  document.getElementById("User_Password").innerHTML += sessionStorage.getItem("Current_Pass");
+  document.getElementById("User_Age").innerHTML += sessionStorage.getItem("Current_User_Age");
+  document.getElementById("User_QuestCoins").innerHTML += sessionStorage.getItem("Current_User_QuestCoin");
+}
 
 
 
 function validateLogin() {
-  console.log("validating");
   let userAttempt = document.getElementById("username").value;
   console.log(userAttempt);
   let passAttempt = document.getElementById("password").value;
   console.log(passAttempt);
   //LOAD THE USERS AND PASSWORDS HERE
-  console.log("before fetch");
   fetch("../user-pass.json")
       .then(response => response.json())
       .then(values => values.forEach(value => {
        if (userAttempt==value.username && passAttempt==value.password) {
-
          console.log("Login validated");
+         sessionStorage.setItem('Current_User',userAttempt);
+         sessionStorage.setItem('Current_Pass',passAttempt);
+         sessionStorage.setItem('Current_User_Unlocked_Background',value.Current_User_Unlocked_Background);
+         sessionStorage.setItem('Current_User_Age',value.age);
+         sessionStorage.setItem('Current_User_QuestCoin',value.QuestCoin);
          openPage('Home');
        }
                     }))
-  console.log('INVALID');
+  document.getElementById("InvalidLogin").style.display = "block";
+  document.getElementById("InvalidLogin").style.margin = "auto";
   //END LOADING THE USER AND PASSWORDS HERE
   
 }
