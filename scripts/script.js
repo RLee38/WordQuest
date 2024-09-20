b34806693854abf101e4ff6bd17f975e55900b66
RLee38 <133907554+RLee38@users.noreply.github.com>
2024-09-20 17:59:43 +0000

    WIP - Added cloud gen to script and flock talk, minor CSS increase, backing up before major speech Beach update

--- scripts/script.js
+++ scripts/script.js
@@ -196,19 +196,58 @@ switch (grade) {
 }
   console.log("Switch case done, word variable assigned");
  
-  while (true) {
+  for (let i = 0; i<5;i++) {
+    console.log("ROUND " +(i+1));
 FTGameLoop(words);
   }
 }
 
 function FTGameLoop(words) {
-  let word = words[Math.random()*words.length];
-  //set the 5 boxes 
-  setTimeout(FTPlaySound(word),1000);
+  //let num = Math.floor(Math.random()*words.length);
+  //console.log(num);
+let num1,num2,num3,num4;
+  let newNums = true;
+  while (newNums) {
+    num1 = Math.floor(Math.random()*words.length);
+    num2 = Math.floor(Math.random()*words.length);
+    num3 = Math.floor(Math.random()*words.length);
+    num4 = Math.floor(Math.random()*words.length);
+    if (num1==num2 || num1==num3 || num1==num4 || num2==num3 || num2==num4 || num3==num4) {
+      console.log("REROLLING");
+    } else {
+      console.log("NUMS FOUND");
+      newNums = false;
+    }
+  }
+  let word = words[num1];
+  console.log("correct word: "+word);
+  let fake1 = words[num2];
+  let fake2 = words[num3];
+  let fake3 = words[num4];
+  console.log("fake words: "+fake1+", "+fake2+", "+fake3);
+//initialize the clouds
+
+  let cloud1 = document.getElementById("cloud1");
+  cloud1.innerHTML = word;
+  let cloud2 = document.getElementById("cloud2");
+  cloud2.innerHTML = word;
+  let cloud3 = document.getElementById("cloud3");
+  cloud3.innerHTML = word;
+  let cloud4 = document.getElementById("cloud4");
+  cloud4.innerHTML = word;
+  
+//Play word sound
+FTPlaySound(word);
+  //
+ 
+
+
+  
+  
 }
 
 function FTPlaySound(word) {
-  let Audio = new Audio("../FlockTalk/Sounds/" + word + ".mp3");
+ // let Audio = new Audio("../FlockTalk/Sounds/" + word + ".mp3");
 }
 
 function newUser() {
@@ -264,6 +303,83 @@ function validateLogin() {
 
 }
 
+/*
+import { Racer } from "./racer.js";
 
+/**
+ * Race
+ */
+/*
+const race = {
+    active: false,
+    time: 0, // How long the race has been going on in centiseconds
+    racers: [],
+    textToType: "This is some generic text that doesn't really mean anything and is just here to extend the typing times.",
 
+    /**
+     * This updates the text to type container using the Player Racer's current position
+     * @param {Racer} playerRacer The player racer that is currently playing
+     */
+/*
+    updateTextToTypeContainer(playerRacer) {
+        let element = document.getElementById("textToTypeContainer");
 
+        element.innerHTML = `<span id="past">${this.textToType.slice(0, playerRacer.typingIndex)}</span><span id="current">${this.textToType.charAt(playerRacer.typingIndex)}</span>${this.textToType.slice(playerRacer.typingIndex + 1, this.textToType.length)}`;
+    }
+};
+
+// Initialize the player racer
+const playerRacer = new Racer("Player", true);
+
+// Append the player racer to the race's list of racers
+race.racers.push(playerRacer);
+
+/**
+ * When the user presses any key
+ */
+/*
+document.addEventListener(("keypress"), (event) => {
+    // If the race is not currently active then return early
+    if (!race.active) return;
+
+    // Process the input
+    if (event.key == race.textToType.charAt(playerRacer.typingIndex)) {
+        // The user types the right key so increase their typing index and update the screen
+        playerRacer.typingIndex++;
+        race.updateTextToTypeContainer(playerRacer);
+
+        // If this player has finished the race then call the racer's finish race method
+        if (playerRacer.typingIndex >= race.textToType.length) playerRacer.finishRace(race.time, race.textToType);
+    } else if (event.key == "Enter") {
+        // The user pressed enter and wants to skip a word
+        playerRacer.useWordSkip(race.textToType);
+        race.updateTextToTypeContainer(playerRacer);
+    } else {
+        // The user pressed the wrong key that is not present in the text to type
+        playerRacer.errorCount++;
+    }
+});
+
+/**
+ * Run every 1 centisecond
+ */
+/*
+setInterval(() => {
+    // If the race is active then increment the race time
+    if (race.active) race.time++;
+}, 10);
+
+/**
+ * Live update the player's current stats throughtout the 
+ * Runs every 1 second
+ */
+/*
+setInterval(() => {
+    // If the race is currently active and the player has not completed their race then update the player stats
+    if (race.active && !playerRacer.completedRace) playerRacer.updateStats(race.time, race.textToType);
+}, 1000);
+
+// Initalize
+race.active = true; // Start the race when the page loads (change later)
+race.updateTextToTypeContainer(playerRacer);
+*/
